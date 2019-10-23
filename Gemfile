6ae01363d964ca0003973a2634ce0fc5b8d2a87f
Chris Oliver <excid3@gmail.com>
2019-10-23 11:25:43 -0500

    Add VCR with webmock for recording HTTP requests automatically

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ group :test do
   gem 'webdrivers'
 
   gem 'webmock'
+  gem 'vcr'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- test/json_ip_test.rb
+++ test/json_ip_test.rb
@@ -1,10 +1,15 @@
 require 'test_helper'
 
 class JsonIpTest < ActiveSupport::TestCase
-  test "returns an IP address" do
-    stub_request(:get, "https://jsonip.org/").
-      to_return(status: 200, body: '{"ip":"1.2.3.4","about":"/about"}', headers: {})
+  setup do
+    VCR.insert_cassette(name)
+  end
 
+  teardown do
+    VCR.eject_cassette
+  end
+
+  test "returns an IP address" do
     assert_equal "1.2.3.4", JsonIp.ip
   end
 end
